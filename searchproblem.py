a3c6712e77cb18c99b0c8dbd97ce066590297378
Jake <jfeinbaum@gmail.com>
2019-11-04 14:37:53 -0500

    started simplesearch implementation

--- searchproblem.py
+++ searchproblem.py
@@ -2,6 +2,13 @@ from snake import Game
 from snake import Snake
 from snake import Action
 
+# Game Dimensions (rows & cols should evenly divide game board)
+WIDTH = 600
+HEIGHT = 600
+ROWS = 30
+COLS = 30
+CELL_SIZE = WIDTH // ROWS
+
 '''
 Abstract class for a generic search problem.
 Does not implement any of the methods.
@@ -11,14 +18,14 @@ class SearchProblem:
     '''
     Returns the start state for the search problem.
     '''
-    def getStartState(self):
+    def get_start_state(self):
         pass
 
     '''
     Input: state, a search state
     Returns: boolean, True if and only if the state is a valid goal state.
     '''
-    def isGoalState(self, state):
+    def is_goal_state(self, state):
         pass
 
     '''
@@ -28,27 +35,29 @@ class SearchProblem:
         action: action required to get to the successor state
         cost: incremental cost of expanding to the successor
     '''
-    def getSuccessors(self, state):
+    def get_successors(self, state):
         pass
 
     '''
     Input: actions, a list of actions to take
     Retrns: total cost of a sequence of (legal) actions
     '''
-    def getCostOfActions(self, actions):
+    def get_cost_of_actions(self, actions):
         pass
 
 
-class simpleSearchProblem(SearchProblem):
+class SimpleSearchProblem(SearchProblem):
 
-    def __init_(self):
-
-
-    def getStartState(self):
+    def __init_(self, starting_state):
+        self.start_state = starting_state
 
 
-    def isGoalState(self, state):
+    def get_start_state(self):
+        return self.start_state
 
-    def getSuccessors(self, state):
+    def is_goal_state(self, state):
+        return len(state['body']) == (ROWS * COLS) - 1
 
-    def getCostOfActions(self, actions):
+    def get_successors(self, state):
+        pass
+
